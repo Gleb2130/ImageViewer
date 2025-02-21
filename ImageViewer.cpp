84b4f6e15c13196d686d8c4f1612f80812049f00
glebvy <gleb.virich9@gmail.com>
2025-02-21 18:15:43 +0100

    Добавьте файлы проекта.

---
+++ ImageViewer.cpp
@@ -0,0 +1,200 @@
+#include "ImageViewer.h"
+
+ImageViewer::ImageViewer(QWidget* parent)
+	: QMainWindow(parent), ui(new Ui::ImageViewerClass)
+{
+	ui->setupUi(this);
+	vW = new ViewerWidget(QSize(500, 500));
+	ui->scrollArea->setWidget(vW);
+
+	ui->scrollArea->setBackgroundRole(QPalette::Dark);
+	ui->scrollArea->setWidgetResizable(true);
+	ui->scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+	ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+	ui->spinBox_2->setRange(20,50);
+	ui->spinBox_2->setValue(20);
+	vW->setObjectName("ViewerWidget");
+	vW->installEventFilter(this);
+}
+
+// Event filters
+bool ImageViewer::eventFilter(QObject* obj, QEvent* event)
+{
+	if (obj->objectName() == "ViewerWidget") {
+		return ViewerWidgetEventFilter(obj, event);
+	}
+	return false;
+}
+
+//ViewerWidget Events
+bool ImageViewer::ViewerWidgetEventFilter(QObject* obj, QEvent* event)
+{
+	ViewerWidget* w = static_cast<ViewerWidget*>(obj);
+
+	if (!w) {
+		return false;
+	}
+
+	if (event->type() == QEvent::MouseButtonPress) {
+		ViewerWidgetMouseButtonPress(w, event);
+	}
+	else if (event->type() == QEvent::MouseButtonRelease) {
+		ViewerWidgetMouseButtonRelease(w, event);
+	}
+	else if (event->type() == QEvent::MouseMove) {
+		ViewerWidgetMouseMove(w, event);
+	}
+	else if (event->type() == QEvent::Leave) {
+		ViewerWidgetLeave(w, event);
+	}
+	else if (event->type() == QEvent::Enter) {
+		ViewerWidgetEnter(w, event);
+	}
+	else if (event->type() == QEvent::Wheel) {
+		ViewerWidgetWheel(w, event);
+	}
+
+	return QObject::eventFilter(obj, event);
+}
+void ImageViewer::ViewerWidgetMouseButtonPress(ViewerWidget* w, QEvent* event)
+{
+	QMouseEvent* e = static_cast<QMouseEvent*>(event);
+	if (e->button() == Qt::LeftButton) {
+		w->setFreeDrawBegin(e->pos());
+		w->setFreeDrawActivated(true);
+	}
+}
+void ImageViewer::ViewerWidgetMouseButtonRelease(ViewerWidget* w, QEvent* event)
+{
+	QMouseEvent* e = static_cast<QMouseEvent*>(event);
+	if (e->button() == Qt::LeftButton && w->getFreeDrawActivated()) {
+		w->freeDraw(e->pos(), QPen(Qt::red));
+		w->setFreeDrawActivated(false);
+	}
+}
+void ImageViewer::ViewerWidgetMouseMove(ViewerWidget* w, QEvent* event)
+{
+	QMouseEvent* e = static_cast<QMouseEvent*>(event);
+	if (e->buttons() == Qt::LeftButton && w->getFreeDrawActivated()) {
+		w->freeDraw(e->pos(), QPen(Qt::red));
+		w->setFreeDrawBegin(e->pos());
+	}
+}
+void ImageViewer::ViewerWidgetLeave(ViewerWidget* w, QEvent* event)
+{
+}
+void ImageViewer::ViewerWidgetEnter(ViewerWidget* w, QEvent* event)
+{
+}
+void ImageViewer::ViewerWidgetWheel(ViewerWidget* w, QEvent* event)
+{
+	QWheelEvent* wheelEvent = static_cast<QWheelEvent*>(event);
+}
+
+//ImageViewer Events
+void ImageViewer::closeEvent(QCloseEvent* event)
+{
+	if (QMessageBox::Yes == QMessageBox::question(this, "Close Confirmation", "Are you sure you want to exit?", QMessageBox::Yes | QMessageBox::No))
+	{
+		event->accept();
+	}
+	else {
+		event->ignore();
+	}
+}
+
+//Image functions
+bool ImageViewer::openImage(QString filename)
+{
+	QImage loadedImg(filename);
+	if (!loadedImg.isNull()) {
+		return vW->setImage(loadedImg);
+	}
+	return false;
+}
+bool ImageViewer::saveImage(QString filename)
+{
+	QFileInfo fi(filename);
+	QString extension = fi.completeSuffix();
+
+	QImage* img = vW->getImage();
+	return img->save(filename, extension.toStdString().c_str());
+}
+
+//Slots
+void ImageViewer::on_actionOpen_triggered()
+{
+	QString folder = settings.value("folder_img_load_path", "").toString();
+
+	QString fileFilter = "Image data (*.bmp *.gif *.jpg *.jpeg *.png *.pbm *.pgm *.ppm .*xbm .* xpm);;All files (*)";
+	QString fileName = QFileDialog::getOpenFileName(this, "Load image", folder, fileFilter);
+	if (fileName.isEmpty()) { return; }
+
+	QFileInfo fi(fileName);
+	settings.setValue("folder_img_load_path", fi.absoluteDir().absolutePath());
+
+	if (!openImage(fileName)) {
+		msgBox.setText("Unable to open image.");
+		msgBox.setIcon(QMessageBox::Warning);
+		msgBox.exec();
+	}
+}
+void ImageViewer::on_actionSave_as_triggered()
+{
+	QString folder = settings.value("folder_img_save_path", "").toString();
+
+	QString fileFilter = "Image data (*.bmp *.gif *.jpg *.jpeg *.png *.pbm *.pgm *.ppm .*xbm .* xpm);;All files (*)";
+	QString fileName = QFileDialog::getSaveFileName(this, "Save image", folder, fileFilter);
+	if (!fileName.isEmpty()) {
+		QFileInfo fi(fileName);
+		settings.setValue("folder_img_save_path", fi.absoluteDir().absolutePath());
+
+		if (!saveImage(fileName)) {
+			msgBox.setText("Unable to save image.");
+			msgBox.setIcon(QMessageBox::Warning);
+		}
+		else {
+			msgBox.setText(QString("File %1 saved.").arg(fileName));
+			msgBox.setIcon(QMessageBox::Information);
+		}
+		msgBox.exec();
+	}
+}
+void ImageViewer::on_actionClear_triggered()
+{
+	vW->clear();
+}
+void ImageViewer::on_actionExit_triggered()
+{
+	this->close();
+}
+
+void ImageViewer::on_pushButtonHello_clicked()
+{
+	vW->drawHelloWorld();
+}
+
+
+void ImageViewer::on_pushButtonVymazat_clicked()
+{
+	vW->clear();
+}
+
+void ImageViewer::on_pushButtonKreslit_clicked()
+{
+	int graf = ui->comboBox->currentIndex();
+	int tGraf = 0;
+
+	if (ui->radioButton->isChecked())
+		tGraf = 0;
+	else if (ui->radioButton_2->isChecked())
+		tGraf = 1;
+	else if (ui->radioButton_3->isChecked())
+		tGraf = 2;
+
+	int delenie = ui->spinBox->value();
+	int N = ui->spinBox_2->value();
+
+	vW->drawGraf(graf, tGraf, delenie, N);
+}
---
+++ ViewerWidget.cpp
@@ -0,0 +1,193 @@
+﻿#include   "ViewerWidget.h"
+
+ViewerWidget::ViewerWidget(QSize imgSize, QWidget* parent)
+	: QWidget(parent)
+{
+	setAttribute(Qt::WA_StaticContents);
+	setMouseTracking(true);
+	if (imgSize != QSize(0, 0)) {
+		img = new QImage(imgSize, QImage::Format_ARGB32);
+		img->fill(Qt::white);
+		resizeWidget(img->size());
+		setPainter();
+	}
+}
+ViewerWidget::~ViewerWidget()
+{
+	delete painter;
+	delete img;
+}
+void ViewerWidget::resizeWidget(QSize size)
+{
+	this->resize(size);
+	this->setMinimumSize(size);
+	this->setMaximumSize(size);
+}
+
+//Image functions
+bool ViewerWidget::setImage(const QImage& inputImg)
+{
+	if (img != nullptr) {
+		delete img;
+	}
+	img = new QImage(inputImg);
+	if (!img) {
+		return false;
+	}
+	resizeWidget(img->size());
+	setPainter();
+	update();
+
+	return true;
+}
+bool ViewerWidget::isEmpty()
+{
+	if (img->size() == QSize(0, 0)) {
+		return true;
+	}
+	return false;
+}
+
+bool ViewerWidget::changeSize(int width, int height)
+{
+	QSize newSize(width, height);
+
+	if (newSize != QSize(0, 0)) {
+		if (img != nullptr) {
+			delete img;
+		}
+
+		img = new QImage(newSize, QImage::Format_ARGB32);
+		if (!img) {
+			return false;
+		}
+		img->fill(Qt::white);
+		resizeWidget(img->size());
+		setPainter();
+		update();
+	}
+
+	return true;
+}
+
+//Draw functions
+void ViewerWidget::freeDraw(QPoint end, QPen pen)
+{
+	painter->setPen(pen);
+	painter->drawLine(freeDrawBegin, end);
+
+	update();
+}
+
+void ViewerWidget::clear()
+{
+	img->fill(Qt::white);
+	update();
+}
+void ViewerWidget::drawGraf(int graf, int gType, int delenie, int N)
+{
+	painter->save();
+
+	painter->setPen(QPen(Qt::black));
+	painter->drawLine(10, 250, 490, 250);
+	painter->drawLine(250, 10, 250, 490);
+
+	float step = 480.0 / delenie;
+
+	for (int i = 0; i <= delenie; i++) {
+		painter->drawLine(i * step + 10, 245, i * step + 10, 255); 
+		painter->drawLine(245, i * step + 10, 255, i * step + 10);
+	}
+
+	float xMin = -M_PI;
+	float xMax = M_PI;
+	float xStep = (xMax - xMin) / N;
+	QVector<QPointF> points;
+
+	for (int i = 0; i <= N; i++) {
+		float x = xMin + i * xStep;
+		float y = (graf == 0) ? sin(x) : cos(x);
+		float xCanvas = 250 + x * 240 / M_PI;  
+		float yCanvas = 250 - y * 200;        
+		points.append(QPointF(xCanvas, yCanvas));
+	}
+	
+
+
+	if (gType == 0) { // Bodový
+		painter->setPen(QPen(Qt::red, 5));
+		for (const QPointF& point : points) {
+			painter->drawPoint(point);
+		}
+	}
+	else if (gType == 1) { // Čiarový
+		painter->setPen(QPen(Qt::red, 2));
+		for (int i = 1; i < points.size(); i++) {
+			painter->drawLine(points[i - 1], points[i]);
+		}
+	}
+	else if (gType == 2) { // Stĺpcový
+		painter->setPen(QPen(Qt::red, 1));
+		for (const QPointF& point : points) {
+			painter->drawLine(point.x(), 250, point.x(), point.y());
+		}
+	}
+
+	painter->restore();
+	update();
+}
+
+void ViewerWidget::drawHelloWorld()
+{
+	//Save default QPainter setting
+	painter->save();
+
+	//H
+	painter->setPen(QPen(Qt::blue));
+	painter->drawLine(20, 20, 20, 120);
+	painter->drawLine(20, 70, 50, 70);
+	painter->drawLine(50, 20, 50, 120);
+	//e
+	painter->drawLine(80, 95, 110, 95);
+	QRect rectangle(60, 70, 50, 50);
+	painter->drawArc(rectangle, 0, 300 * 16);
+	//l
+	painter->drawLine(120, 20, 120, 120);
+	//l
+	painter->drawLine(130, 20, 130, 120);
+	//o
+	painter->drawEllipse(QPointF(170, 95), 25, 25);
+
+	//W
+	const QPointF points[5] = {
+		QPointF(20,140),
+		QPointF(30,240),
+		QPointF(40,190),
+		QPointF(50,240),
+		QPointF(60,140)
+	};
+	painter->drawPolyline(points, 5);
+	//o
+	painter->setBrush(QBrush(Qt::blue));
+	painter->drawEllipse(QPointF(85, 215), 25, 25);
+	//r
+	painter->setFont(QFont("Arial", 50));
+	painter->drawText(QPoint(120, 235), "r");
+	//l
+	painter->fillRect(150, 140, 10, 100, QBrush(QColor(0, 255, 100)));
+	//d
+	painter->setFont(QFont("Arial", 50));
+	painter->drawText(QPoint(170, 235), "d");
+
+	//Restore default QPainter setting
+	painter->restore();
+	update();
+}
+
+//Slots
+void ViewerWidget::paintEvent(QPaintEvent* event)
+{
+	QPainter painter(this);
+	QRect area = event->rect();
+	painter.drawImage(area, *img, area);
+}
